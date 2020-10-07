c55c6f29fa24be5557ccdf0af607e5ebb4fe4b23
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2020-10-07 19:34:50 +0100

    Update

--- index.js
+++ index.js
@@ -2,15 +2,21 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const moment = require('moment');
+
+const formattedDating = moment(new Date()).format('MMMM Do YYYY, h:mm:ss a');
+console.log(formattedDating);
 
 //Run when client connects
 io.on('connection', (socket) => {
   socket.on('disconnect', () => {
-    socket.nickname = 'nickname';
+    //socket.nickname = 'nickname';
+    console.log(`${socket.nickname} left`);
     io.emit('users-changed', { user: socket.nickname, event: 'left' });
   });
 
   socket.on('set-nickname', (nickname) => {
+    console.log(nickname);
     socket.nickname = nickname;
     io.emit('users-changed', { user: nickname, event: 'joined' });
   });
@@ -19,7 +25,7 @@ io.on('connection', (socket) => {
     io.emit('message', {
       text: message.text,
       from: socket.nickname,
-      created: new Date(),
+      created: formattedDating,
     });
   });
 });
